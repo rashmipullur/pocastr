be94ca8a6ed050d1583532dc3cdbb92dca1f6217
rashmipullur <mheresince1998@gmail.com>
2024-06-06 23:07:08 +0530

    clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://fond-caiman-64.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
